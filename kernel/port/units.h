bb21eb7543eabac23f51f447a7403db56bda8125
Ian Denhardt <ian@zenhack.net>
2012-03-17 22:03:06 -0400

    We have a handle to the end of the kernel image; this will be useful later.

---
+++ kernel/port/units.h
@@ -0,0 +1,8 @@
+#ifndef KERNEL_PORT_UNITS_H
+#define KERNEL_PORT_UNITS_H
+
+#define KIBI 1024
+#define MEBI (KIBI * KIBI)
+#define GIBI (KIBI * MEBI)
+
+#endif
--- kernel/x86/arch_main.c
+++ kernel/x86/arch_main.c
@@ -1,12 +1,17 @@
 #include <kernel/x86/multiboot.h>
 #include <kernel/port/stdio.h>
 #include <kernel/x86/serial.h>
+#include <kernel/port/units.h>
+
+/* defined in link.ld; located at the end of the kernel image. */
+extern void *kend;
 
 void arch_main(MultiBootInfo *mb_info) {
 	FILE com1;
 	serial_init(COM1, &com1);
 	stdout = &com1;
 	printf("Hello, World!\n");
-	printf("Mboot info at : 0x%x, high-memory: 0x%x\n", mb_info, mb_info->mem_upper);
+	printf("Mboot info at : 0x%x, high-memory: 0x%x\n", mb_info, mb_info->mem_upper * KIBI);
+	printf("kernel end at : 0x%x", &kend);
 	while(1);
 }
