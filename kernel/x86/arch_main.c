f5b7cf5bcbe643d3502d39d1d47aa42ac06c1143
Ian Denhardt <ian@zenhack.net>
2012-03-17 20:54:46 -0400

    Get memory map from mboot.

--- kernel/x86/arch_main.c
+++ kernel/x86/arch_main.c
@@ -1,10 +1,12 @@
+#include <kernel/x86/multiboot.h>
 #include <kernel/port/stdio.h>
 #include <kernel/x86/serial.h>
 
-void arch_main(void) {
+void arch_main(MultiBootInfo *mb_info) {
 	FILE com1;
 	serial_init(COM1, &com1);
 	stdout = &com1;
 	printf("Hello, World!\n");
+	printf("Mboot info at : 0x%x, high-memory: 0x%x\n", mb_info, mb_info->mem_upper);
 	while(1);
 }
--- kernel/x86/multiboot.h
+++ kernel/x86/multiboot.h
@@ -7,4 +7,17 @@
 #define MULTIBOOT_MAGIC 0x1badb002
 #define MULTIBOOT_CHKSUM(flags) (-(MULTIBOOT_MAGIC + flags))
 
+#define MULTIBOOT_MEMINFO (1<<1)
+
+#ifndef ASM_FILE
+
+typedef struct MultiBootInfo MultiBootInfo;
+struct MultiBootInfo {
+	long flags;
+	long mem_lower;
+	long mem_upper;
+	/* there are more fields, but we aren't interested. (yet.) */
+};
+
+#endif
 #endif
