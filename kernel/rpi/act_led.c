58586997a0dd47821f43a96d10da73f2c6623549
Ian Denhardt <ian@zenhack.net>
2013-10-06 19:44:12 -0400

    Initial work on the raspberry pi.
    Not yet integrated into the the rest of the build system.
    
    So far, all this does is boot, set up a stack, and call a c routine
    which repeatedly outputs "hello world" to the activity light using morse
    code.
    
    The morse code output will probably not stick around very long.

---
+++ kernel/rpi/act_led.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+#define IO_BASE 0x20000000
+#define GPIO_BASE (IO_BASE + 0x00200000)
+
+void act_led_on(void) {
+	// enable output to the led:
+	*((uint32_t*)(GPIO_BASE + 4)) = (1<<18);
+	// turn the led on:
+	*((uint32_t*)(GPIO_BASE + 40)) = (1<<16);
+}
+
+void act_led_off(void) {
+	*((uint32_t*)(GPIO_BASE + 4)) = (1<<18);
+	*((uint32_t*)(GPIO_BASE + 28)) = (1<<16);
+}
---
+++ kernel/rpi/act_led.h
@@ -0,0 +1,3 @@
+
+void act_led_on(void);
+void act_led_off(void);
