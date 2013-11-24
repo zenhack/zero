6bc934aea1253b2d2ecde93a1097e2b60c16331a
Ian Denhardt <ian@zenhack.net>
2013-11-24 13:44:51 -0500

    RPI: use symbolic names in act_led.c
    Previously had hard coded numbers.

--- kernel/rpi/act_led.c
+++ kernel/rpi/act_led.c
@@ -4,14 +4,18 @@
 
 #define GPIO_BASE (IO_BASE + 0x00200000)
 
+#define GPFSEL1 (GPIO_BASE + 4)
+#define GPSET0 (GPIO_BASE + 28)
+#define GPCLR0 (GPIO_BASE + 40)
+
 void act_led_on(void) {
 	// enable output to the led:
-	*((uint32_t*)(GPIO_BASE + 4)) = (1<<18);
+	*((uint32_t*)GPFSEL1) = (1<<18);
 	// turn the led on:
-	*((uint32_t*)(GPIO_BASE + 40)) = (1<<16);
+	*((uint32_t*)GPCLR0) = (1<<16);
 }
 
 void act_led_off(void) {
-	*((uint32_t*)(GPIO_BASE + 4)) = (1<<18);
-	*((uint32_t*)(GPIO_BASE + 28)) = (1<<16);
+	*((uint32_t*)GPFSEL1) = (1<<18);
+	*((uint32_t*)GPSET0) = (1<<16);
 }
