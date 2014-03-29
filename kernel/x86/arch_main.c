fc0f099db99c70b1b5de2acd0c0a135d4f79fe57
Ian Denhardt <ian@zenhack.net>
2014-03-28 23:47:04 -0400

    cleaned up arch_main on x86
    It was full of stale ad-hoc tests, and difficult to read. It's been
    cleaned up, and now has a more clear initialization path.

--- kernel/x86/arch_main.c
+++ kernel/x86/arch_main.c
@@ -17,88 +17,40 @@
 /* defined in link.ld; located at the end of the kernel image. */
 extern void *kend;
 
-void breakpoint_handler(Regs *regs) {
-	printf("A wild breakpoint appeared!\n");
-}
-
-void thread1(void *other) {
-	while(1) {
-		printf("Thread 1 !!!\n");
-		other = yield(other);
-	}
-}
-
-void thread2(void *other) {
-	while(1) {
-		printf("Thread 2 !!!\n");
-		break;
-		other = yield(other);
-	}
-}
-
 void arch_main(MultiBootInfo *mb_info) {
-	void *other_thread;
 	SerialPort com1;
 	FILE console;
 	MuxWriter mux_writer;
-	CPUInfo cpu_info;
-	char cpu_vendor[13];
 	uint32_t apic_id;
-	int i;
-
-	uintptr_t alignptr;
 
 	gdt_init();
+
+	/* direct output to both the serial line and video console. */
 	serial_init(COM1, &com1);
 	text_console_init(&console);
 	muxwriter_init(&mux_writer, (FILE*)&com1, &console);
 	stdout = (FILE*)&mux_writer;
+
 	idt_init();
-	register_int_handler(0x3, breakpoint_handler);
-	asm volatile("int $0x3");
-	printf("Hello, World!\n");
-	printf("Mboot info at : 0x%x, high-memory: 0x%x\n", mb_info, mb_info->mem_upper * KIBI);
-	printf("kernel end at : 0x%x\n", &kend);
+
+	printf("Booting Zero...\n");
+	printf("\n");
+	printf("Mutliboot info struct located at 0x%x\n", mb_info);
+	printf("  High memory : 0x%x\n", mb_info->mem_upper * KIBI);
+	printf("  Kernel image ends at : 0x%x\n", &kend);
+
+	/* initialize the heap, using the chunk of memory between the
+	 * kernel and mem_upper. */
 	heap_init((uintptr_t)&kend, (uintptr_t)mb_info->mem_upper * KIBI);
 	
-	for(i = 0; i < 1000; i++) {
-		char *x, *y, *z;
-		x = kalloc(512);
-		y = kalloc(512);
-		z = kalloc(512);
-		if(!(x && y && z)) {
-			printf("Out of memory!\n");
-		}
-		kfree(x, 512);
-		kfree(z, 512);
-		kfree(y, 512);
+	if(!have_apic()) {
+		panic("No apic found!\n");
 	}
-	printf("Ok!\n");
-
-	alignptr = (uintptr_t)kalloc_align(37, 0x100);
-	printf("alignptr : 0x%x (should end in 00)\n", alignptr);
-
-	memset(&cpu_info, 0, sizeof(CPUInfo));
-	cpuid(&cpu_info);
-	memcpy(&cpu_vendor[0], &cpu_info.ebx, sizeof(uint32_t));
-	memcpy(&cpu_vendor[4], &cpu_info.edx, sizeof(uint32_t));
-	memcpy(&cpu_vendor[8], &cpu_info.ecx, sizeof(uint32_t));
-	cpu_vendor[12] = '\0';
-	printf("Cpu: %s\n", cpu_vendor);
-	
-	if(!have_apic())
-		panic("No apic found!");
 	apic_id = get_apic_id();
 	enable_apic();
 	printf("Apic ID #%d is online.\n", apic_id);
 
 	paging_init(mb_info->mem_upper * KIBI);
-	other_thread = mk_thread(4 * KIBI, thread2);
-	if(other_thread) {
-		thread1(other_thread);
-	} else {
-		printf("kalloc_failed in %s near line %d!\n", __FILE__, __LINE__);
-	}
 
 	while(1);
 }
