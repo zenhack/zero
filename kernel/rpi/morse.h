e3085e72c77799db209602e973117ffbe4e29fe4
Ian Denhardt <ian@zenhack.net>
2014-04-18 00:39:15 -0400

    RPI: moved sleep function and morse code stuff to their own modules.

---
+++ kernel/rpi/morse.h
@@ -0,0 +1,9 @@
+#ifndef KERNEL_RPI_MORSE_H
+#define KERNEL_RPI_MORSE_H
+
+#define MORSE_UNIT 0x3f0000
+
+void morse_string(char *s);
+void sleep(uint32_t time);
+
+#endif
---
+++ kernel/rpi/sleep.h
@@ -0,0 +1,6 @@
+#ifndef KERNEL_RPI_SLEEP_H
+#define KERNEL_RPI_SLEEP_H
+
+void sleep(uint32_t time);
+
+#endif
