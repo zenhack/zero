338f7f8d5ab351e12825741d0fbb07900d20b86f
Ian Denhardt <ian@zenhack.net>
2014-04-18 01:23:21 -0400

    RPI: Removed a leftover prototype of sleep frome morse.h

--- kernel/rpi/morse.h
+++ kernel/rpi/morse.h
@@ -1,12 +1,10 @@
 #ifndef KERNEL_RPI_MORSE_H
 #define KERNEL_RPI_MORSE_H
 
-#include <stdint.h>
 #include <stddef.h>
 
 #define MORSE_UNIT 0x3f0000
 
 void morse_write(char *s, size_t len);
-void sleep(uint32_t time);
 
 #endif
--- kernel/rpi/sleep.h
+++ kernel/rpi/sleep.h
@@ -1,6 +1,8 @@
 #ifndef KERNEL_RPI_SLEEP_H
 #define KERNEL_RPI_SLEEP_H
 
+#include <stdint.h>
+
 void sleep(uint32_t time);
 
 #endif
