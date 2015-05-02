4e97a2e27060a4a7daae5e7c4bea357b681ea3a2
Ian Denhardt <ian@zenhack.net>
2015-05-01 23:30:47 -0400

    Add qemu convienence targets to x86 makefile

--- kernel/x86/main.mk
+++ kernel/x86/main.mk
@@ -1,1 +1,8 @@
 all: $(objdir)/kernel.x86.elf
+
+# Boot the kernel in qemu:
+qemu-run: all
+	qemu-system-i386 -kernel $(objdir)/kernel.x86.elf -serial stdio
+# Same thing, but wait for gdb to attach:
+qemu-gdb: all
+	qemu-system-i386 -kernel $(objdir)/kernel.x86.elf -serial stdio -s -S
