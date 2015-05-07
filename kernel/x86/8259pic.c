7843a48e864bbab80b04fba7e7386ed51278c7f8
Ian Denhardt <ian@zenhack.net>
2015-05-06 23:02:56 -0400

    x86/8259pic: Update docs + slight refactoring
    The comments in remap no longer talk about "we're going to disable it anyway,"
    since we plan to actually use the PIC for a few things first.
    
    Additionally, a helper macro for IRQ numbers has been defined.

--- kernel/x86/8259pic.c
+++ kernel/x86/8259pic.c
@@ -12,14 +12,10 @@
 #define PIC_EOI 0x20
 
 void remap_8259pic(void) {
-	/* Even though we're disabling this in favor of the apic, it's
-	 * apparently still possible for the 8259pic to generate spurrious
-	 * interupts, so we should remap these to things that don't conflict
-	 * with the CPU's exceptions. */
 	out8(PIC_CMD(PIC_MASTER), PIC_INIT);
 	out8(PIC_CMD(PIC_SLAVE), PIC_INIT);
-	out8(PIC_DATA(PIC_MASTER), 0x20); /* last cpu exception is at 0x1f */
-	out8(PIC_DATA(PIC_SLAVE), 0x28); /* right after the master pic */
+	out8(PIC_DATA(PIC_MASTER), IRQ(0)); /* Set the starting intterupt of the master */
+	out8(PIC_DATA(PIC_SLAVE), IRQ(8)); /* ...and the slave. */
 
 	/* Tell the pics about their relationship. The slave is on the master's
 	 * pin #2. */
--- kernel/x86/8259pic.h
+++ kernel/x86/8259pic.h
@@ -1,7 +1,15 @@
 #ifndef KERNEL_X86_8259PIC_H
 #define KERNEL_X86_8259PIC_H
 
+/* Interrupt numbers for the IRQs. These start at 0x20, since the last
+ * CPU exception is 0x1f. */
+#define IRQ(N) ((N)+0x20)
+
+/* Remap the PIC to avoid collisions with the CPU exceptions.
+ * See [osdev/PIC]. */
 void remap_8259pic(void);
+
+/* Turn off the PIC. Must call remap first. */
 void disable_8259pic(void);
 
 #endif
