ca24a8c508da7c15683fc84a79a2960afc194742
Ian Denhardt <ian@zenhack.net>
2015-06-11 01:22:35 -0400

    mk_thread: whoops, stack grows down
    This doesn't seem to actually fix the problem though. For some reason,
    when example_thread is called, esp is nowhere near what mk_thread is
    setting it to. Further investigation required.

--- kernel/x86/thread.c
+++ kernel/x86/thread.c
@@ -12,11 +12,14 @@ Thread *mk_thread(size_t stack_size, void (*entry)(void *), void *data) {
 		if(ret) kfree(ret, sizeof(Thread));
 		return NULL;
 	}
-	ret->regs.esp = (uint32_t)&stack[1];
-	ret->regs.ebp = 0;
+	stack = (uint32_t*)(((size_t)stack) + stack_size);
+	stack[-1] = 0; /* end of the ebp "linked list" for stack traces. */
+	stack[-2] = (uint32_t)data;
+	stack[-3] = 0; /* This is the ret address, but threads should *never* return. */
+	ret->regs.ebp = (uint32_t)&stack[-2];
+	ret->regs.esp = (uint32_t)&stack[-4];
+
 	ret->regs.eip = (uint32_t)entry;
-	stack[0] = (uint32_t)data;
-	stack[1] = 0; /* This is the ret address, but threads should *never* return. */
 	ret->regs.ds = SEGOFF(KDATA_SEGMENT);
 	ret->regs.ss = SEGOFF(KDATA_SEGMENT);
 	ret->regs.cs = SEGOFF(KCODE_SEGMENT);
