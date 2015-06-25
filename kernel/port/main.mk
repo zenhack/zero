eed7d09ad23e0a1e8f9a0fac8ecf24069fa68dee
Ian Denhardt <ian@zenhack.net>
2015-06-25 19:07:31 -0400

    Add crude unit test, catches kalloc bug
    The kalloc_align calls at the end are returning the same address.

--- kernel/port/main.mk
+++ kernel/port/main.mk
@@ -5,3 +5,15 @@ csrc += \
 	kernel/port/panic.c\
 	kernel/port/stdio.c\
 	kernel/port/string.c
+
+tests += \
+	kernel/port/heap.test
+
+%.test: %.test_o
+	cc -o $@ $<
+%.test_o: test_%.c
+	cc -I $(srcdir) -c -o $@ $<
+check: $(tests)
+	for t in $(tests); do \
+		./$$t ; \
+	done
