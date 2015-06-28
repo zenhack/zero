5df803e7c89d85bf0461e8738597bcc18c8645a2
Ian Denhardt <ian@zenhack.net>
2015-06-28 00:42:40 -0400

    Rewrite heap allocator
    I did this in an effort to make things more readable. It's broken
    though, and I'm not sure it was a good idea. It's failing the test
    earlier than the last one. I'm committing it to a branch so I don't lose
    it, but I may or may not actually merge.

--- kernel/port/heap.c
+++ kernel/port/heap.c
@@ -1,179 +1,138 @@
 #include <stddef.h>
 #include <kernel/port/heap.h>
 #include <kernel/port/units.h>
-#include <kernel/port/stdio.h>
-#include <kernel/port/panic.h>
 
-typedef union Bumper Bumper;
-union Bumper {
-	uintptr_t raw;
-	struct {
-		uintptr_t used : 1;
-		uintptr_t size : BITS(BYTE) * sizeof(uintptr_t) - 1;
-	} v;
+typedef struct Bumper Bumper;
+struct Bumper {
+	uintptr_t used : 1;
+	uintptr_t size : BITS(sizeof(uintptr_t)) - 1;
 };
 
 static struct {
-	Bumper *start;
-	Bumper *end;
+	Bumper *start, *end;
 } heap;
 
-
-/* round_up/round_down return `n` rounded to the nearest value that is zero
- * modulo `align`. the difference should be clear from the names. */
-static uintptr_t round_up(uintptr_t n, uintptr_t align) {
-	if (n % align) {
-		return n + (align - (n % align));
-	}
-	return n;
-}
-
 static uintptr_t round_down(uintptr_t n, uintptr_t align) {
-	return n - n % align;
+	return n - (n % align);
 }
 
-static Bumper *get_head(Bumper *foot) {
-	return (Bumper*) ((((uintptr_t)foot) - foot->v.size) + sizeof(Bumper));
+static uintptr_t round_up(uintptr_t n, uintptr_t align) {
+	if (n % align == 0) {
+		return n;
+	}
+	return round_down(n, align) + align;
 }
 
 static Bumper *get_foot(Bumper *head) {
-	return (Bumper*)((((uintptr_t)head) + head->v.size) - sizeof(Bumper));
+	return (Bumper *)((uintptr_t)head + head->size) - 1;
+}
+
+static Bumper *get_head(Bumper *foot) {
+	return (Bumper *)((uintptr_t)foot - foot->size) + 1;
+}
+
+static Bumper *align_head(Bumper *head, uintptr_t align) {
+	return (Bumper *)round_up((uintptr_t)head, align) - 1;
+}
+
+static int has_aligned_space(Bumper *head, uintptr_t size, uintptr_t align) {
+	Bumper *aligned_head = align_head(head, align);
+	uintptr_t size_loss = (uintptr_t)aligned_head - (uintptr_t)head;
+	return head->size - size_loss >= size;
+}
+
+static void make_block(Bumper *head, Bumper *foot, uintptr_t used) {
+	uintptr_t size = (uintptr_t)(foot + 1) - (uintptr_t)head;
+	head->size = size;
+	head->used = used;
+	*foot = *head;
 }
 
 void heap_init(uintptr_t start, uintptr_t end) {
-	Bumper *free_foot;
+	/* make sure the heap is properly aligned */
+	heap.start = (Bumper *)round_up((uintptr_t)start, sizeof(Bumper));
+	heap.end   = (Bumper *)round_down((uintptr_t)end, sizeof(Bumper));
 
-	/* align the start and end of the heap. */
-	heap.start = (Bumper*)round_up(start, sizeof(uintptr_t));
-	heap.end = (Bumper*)round_down(end, sizeof(uintptr_t));
-
-	/* prolouge and epilogue blocks */
-	heap.start->v.used = 1;
+	/* Bound the heap with things that look like "used" blocks; this way we
+	 * don't need any special case logic when merging adjacent blocks on a
+	 * free. */
+	heap.start->used = 1;
 	heap.start++;
-
+	heap.end->used = 1;
 	heap.end--;
-	heap.end->v.used = 1;
-	
-	/* initial free block */
-	heap.start->v.used = 0;
-	heap.start->v.size = ((uintptr_t)heap.end) - ((uintptr_t)heap.start);
-	free_foot = get_foot(heap.start);
-	free_foot->raw = heap.start->raw;
+
+	/* Now make the rest one giant free block */
+	make_block(heap.start, heap.end, 0);
 }
 
-/* Returns a true value if the block with header `head` is large enough to
- * store a value of size `space` on an `align` byte boundary. Returns false
- * otherwise. */
-static int has_aligned_space(Bumper *head, uintptr_t space, uintptr_t align) {
-	/* round up to the nearest `align` byte boundary, find the difference
-	 * between that and the start of the block, and subtract that from the
-	 * total size. */
-	return 
-		head->v.size
-		- (
-			round_up((uintptr_t)(head+1),align)
-			- (uintptr_t)(head+1)
-		) >= space;
+void _kfree(void *ptr) {
+	Bumper *head = (Bumper *)ptr - 1;
+	Bumper *foot = get_foot(head);
+
+	Bumper *left_foot = foot - 1;
+	Bumper *right_head = foot + 1;
+
+	if(!left_foot->used) {
+		head = get_head(left_foot);
+	}
+	if(!right_head->used) {
+		foot = get_foot(right_head);
+	}
+
+	make_block(head, foot, 0);
 }
 
-void *kalloc_align(uintptr_t size, uintptr_t alignment) {
-	Bumper *head, *foot, *new_foot, *new_head;
+void kfree(void *ptr, uintptr_t size) {
+	return _kfree(ptr);
+}
 
-	/* we're not allocating units less than sizeof(uintptr_t); round up
-	 * to the nearest such unit and adjust for the header/footer. */
-	size = round_up(size, sizeof(uintptr_t));
+
+void *kalloc_align(uintptr_t size, uintptr_t align) {
+	/* regardless of what the user asks for, we always need to align at
+	 * least on a bumper-sized boundary. This also means we're allocating
+	 * whole-bumper sized objects. */
+	size  = round_up(size, sizeof(Bumper));
+	align = round_up(size, sizeof(Bumper));
+
+	/* make space for the bumpers themselves: */
 	size += 2 * sizeof(Bumper);
 
-	/* find a block */
-	head = heap.start;
-	while((head < heap.end) &&
-		(head->v.used ||
-		!has_aligned_space(head,size,alignment))) {
-
+	/* find the first block that will fit */
+	Bumper *head = heap.start;
+	while(head < heap.end && !has_aligned_space(head, size, align)) {
 		head = get_foot(head) + 1;
 	}
-
-	/* no room for an allocation of this size. */
-	if(head >= heap.end)
+	if(head >= heap.end) {
+		/* Out of luck */
 		return NULL;
-
-	new_head = (Bumper*)round_up((uintptr_t)(head+1), alignment);
-	new_head--;
-
-	/* Start of block isn't right for alignment. */
-	if(new_head != head) {
-		/* we need to split the block so that kfree will find the
-		 * bumpers. */
-		foot = new_head - 1;
-		head->v.size -= size;
-		foot->raw = head->raw;
-
-		new_head->v.size = size;
-		new_head->v.used = 1;
-		new_foot = get_foot(new_head);
-		new_foot->raw = new_head->raw;
-		return new_head + 1;
 	}
 
-	/* alignment is okay : */
-	
-	/* not enough room to split, just return the whole thing. */
-	if(head->v.size <= size + 2 * sizeof(Bumper)) {
-		head->v.used = 1;
-		foot = get_foot(head);
-		foot->raw = head->raw;
-		return head + 1;
+	/* align the head */
+	Bumper *foot = get_foot(head);
+	Bumper *aligned_head = align_head(head, align);
+
+	/* turn any leading excess into it's own (free) block. Note that it is
+	 * possible for this block to be too small to be usable, but it doesn't
+	 * cause a correctness problem. Even if the head and foot end up being
+	 * the same piece of memory, everything still works. */
+	if(head != aligned_head) {
+		Bumper *left_foot = aligned_head - 1;
+		make_block(head, left_foot, 0);
 	}
 
-	/* room for at least one byte; split the block. */
-	foot = get_foot(head);
-	foot->v.size -= size;
-	head->v.size = size;
-	new_head = get_head(foot);
-	new_foot = get_foot(head);
-	head->v.used = 1;
-	new_foot->raw = head->raw;
-	new_head->raw = foot->raw;
-	return head + 1;
+	/* set up the footer at the right place. */
+	aligned_head->size = size;
+	Bumper *aligned_foot = get_foot(aligned_head);
+	make_block(aligned_head, aligned_foot, 1);
+
+	/* Same thing, turn trailing excess into its own block. */
+	if(foot != aligned_foot) {
+		Bumper *right_head = aligned_foot + 1;
+		make_block(right_head, foot, 0);
+	}
+	return aligned_head + 1;
 }
 
 void *kalloc(uintptr_t size) {
 	return kalloc_align(size, sizeof(uintptr_t));
-}
-
-void kfree(void *ptr, uintptr_t size) {
-	Bumper *head, *foot, *foot_left, *head_right;
-
-	size = round_up(size, sizeof(uintptr_t));
-	size += 2 * sizeof(Bumper);
-
-	head = ptr;
-	head--;
-
-	/* sanity checks */
-	if(!head->v.used) {
-		panic("double free");
-	}
-	if(head->v.size < size || head->v.size > size + 2 * sizeof(Bumper)) {
-		panic("size mismatch in free!");
-	}
-
-	foot = get_foot(head);
-
-	foot_left = head - 1;
-	head_right = foot + 1;
-
-	if(!foot_left->v.used) {
-		size += foot_left->v.size;
-		head = get_head(foot_left);
-	}
-
-	if(!head_right->v.used) {
-		size += head_right->v.size;
-		foot = get_foot(head_right);
-	}
-
-	head->v.used = 0;
-	head->v.size = size;
-	foot->raw = head->raw;
 }
