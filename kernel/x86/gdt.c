dcbb3415886ae15dfb7d7b5a58497c0cbe1aa352
Ian Denhardt <ian@zenhack.net>
2015-08-01 12:25:11 -0400

    Define the GDT statically in the binary
    ...rather than constructing it at runtime. This is mostly just an
    experiment. It seems to work okay.

--- kernel/x86/gdt.c
+++ kernel/x86/gdt.c
@@ -10,11 +10,28 @@
 #define FLAG_GR_PAGE (1<<3)
 #define FLAG_SZ_32 (1<<2)
 
+/**
+ * Expands to a struct literal describing a GDTEnt with the given parameters.
+ * This lets us define the GDT statically in the binary.
+ *
+ * Note: Some of the arguments are used more than once within the body of the
+ * macro, so only constant expressions may be used.
+ */
+#define MKGDT(base, limit, access_bits, flags) \
+	{ \
+		.limit_low = ((limit) & 0xffff), \
+		.base_low = ((base) & 0xffff), \
+		.base_mid = ((base) >> 16) & 0xff, \
+		.access = (access_bits) | ACCESS_ALWAYS1, \
+		.flags_limit_high = ((flags) << 4) | (((limit) >> 16) & 0x0f), \
+		.base_high = ((base) >> 24) & 0xff \
+	}
+
 typedef struct GDTDesc GDTDesc;
 typedef struct GDTEnt GDTEnt;
 
 struct GDTDesc {
-	uint16_t size;	
+	uint16_t size;
 	uint32_t offset;
 }__attribute__((packed));
 
@@ -27,33 +44,23 @@ struct GDTEnt {
 	uint8_t base_high;
 }__attribute__((packed));
 
-GDTDesc gdt_desc;
-GDTEnt gdt[NUM_GDTENT];
+GDTEnt gdt[NUM_GDTENT] = {
+	MKGDT(0, 0, 0, 0),
+	MKGDT(0, ~0,
+		ACCESS_PRESENT|ACCESS_PRIVL(0)|ACCESS_RW|ACCESS_EX,
+		FLAG_GR_PAGE|FLAG_SZ_32),
+	MKGDT(0, ~0,
+		ACCESS_PRESENT|ACCESS_PRIVL(0)|ACCESS_RW,
+		FLAG_GR_PAGE|FLAG_SZ_32),
+};
+GDTDesc gdt_desc = {
+	.size = NUM_GDTENT * sizeof(GDTEnt) - 1,
+	.offset = (uint32_t)&gdt[0],
+};
 
 /* Actually loads the gdt. in gdt_load.S */
 extern void gdt_load(GDTDesc *desc);
 
-void set_gdtent(GDTEnt *ent, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
-	ent->limit_low = (uint16_t)(limit & 0xffff);
-	ent->base_low = (uint16_t)(base & 0xffff);
-	ent->base_mid = (uint8_t)((base >> 16) & 0xff);
-	ent->access = access | ACCESS_ALWAYS1;
-	ent->flags_limit_high = (flags << 4);
-	ent->flags_limit_high |= (uint8_t)((limit >> 16) & 0x0f);
-	ent->base_high = (uint8_t)((base >> 24) & 0xff);
-}
-
 void gdt_init(void) {
-	set_gdtent(&gdt[NULL_SEGMENT], 0, 0, 0, 0);
-	set_gdtent(&gdt[KCODE_SEGMENT], 0, ~0,
-		ACCESS_PRESENT|ACCESS_PRIVL(0)|ACCESS_RW|ACCESS_EX,
-		FLAG_GR_PAGE|FLAG_SZ_32);
-	set_gdtent(&gdt[KDATA_SEGMENT], 0, ~0,
-		ACCESS_PRESENT|ACCESS_PRIVL(0)|ACCESS_RW,
-		FLAG_GR_PAGE|FLAG_SZ_32);
-
-	gdt_desc.size = NUM_GDTENT * sizeof(GDTEnt) - 1;
-	gdt_desc.offset = (uint32_t)&gdt[0];
-
 	gdt_load(&gdt_desc);
 }
