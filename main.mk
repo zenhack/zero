176323568a9557e0d3eff93f8151d0a333140664
Ian Denhardt <ian@zenhack.net>
2015-08-12 20:11:05 -0400

    Use cpp's -M to generate complete dependencies

--- main.mk
+++ main.mk
@@ -18,7 +18,9 @@ LIBS = -lgcc
 
 linker_script = $(srcdir)/kernel/$(platform)/link.ld
 
-objects = $(ssrc:.S=.o) $(csrc:.c=.o)
+objects := $(ssrc:.S=.o) $(csrc:.c=.o)
+
+depfiles := $(objects:.o=.deps.mk)
 
 kernel.$(platform).elf: $(objects) $(linker_script)
 	$(LD) -o $@ $(objects) $(CFLAGS) $(LDFLAGS) $(LIBS) -T $(linker_script)
@@ -27,9 +29,17 @@ clean:
 	rm -f \
 		$(shell find * -name '*.o') \
 		kernel.*.elf \
+		$(depfiles) \
 		$(cleanfiles)
 
-%.o: %.S
+%.o: %.S %.deps.mk
 	$(CC) $(CFLAGS) -DASM_FILE -c -o $@ $<
-%.o: %.c
+%.o: %.c %.deps.mk
 	$(CC) $(CFLAGS) -c -o $@ $<
+
+%.deps.mk: %.S
+	$(CPP) -M -MQ $(@:.deps.mk=.o) $(CFLAGS) -DASM_FILE $< > $@
+%.deps.mk: %.c
+	$(CPP) -M -MQ $(@:.deps.mk=.o) $(CFLAGS)  $< > $@
+
+include $(depfiles)
