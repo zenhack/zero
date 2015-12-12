1cafd58ec59e7bdba78b1a8889ff4061d26e0cb9
Ian Denhardt <ian@zenhack.net>
2015-12-12 16:20:42 -0500

    act_led: Add some comments re: pin number.

--- kernel/platforms/rpi/act_led.c
+++ kernel/platforms/rpi/act_led.c
@@ -3,8 +3,13 @@
 #include <kernel/rpi/gpio.h>
 #include <kernel/port/mmio.h>
 
-/* The GPIO pin assigned to the led: */
+/* The GPIO pin assigned to the led. The RPI B+ and B2 supposedly use a
+ * different pin that the original RPI. Neither one of these actually seems
+ * to work on my B2 though. Pin 16 is for the older models, which is
+ * currently enabled. Pin 47 is supposed to be the right value for newer
+ * models. */
 #define LED_PIN 16
+/* #define LED_PIN 47 */
 
 /* Each of these works as follows:
  *
