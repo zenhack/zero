0824da39fb0762bf1022e7f41f55eaaec17092be
Ian Denhardt <ian@zenhack.net>
2015-12-17 08:24:05 -0500

    ACPI: define struct for SDT headers.

--- kernel/platforms/x86/include/kernel/x86/acpi.h
+++ kernel/platforms/x86/include/kernel/x86/acpi.h
@@ -18,6 +18,18 @@ struct acpi_RSDP {
 	uint8_t _reserved[3];
 }__attribute__((packed));
 
+struct acpi_SDTHeader {
+	uint8_t signature[4];
+	uint32_t length;
+	uint8_t revision;
+	uint8_t checksum;
+	uint8_t oemid[6];
+	uint8_t oem_table_id[8];
+	uint32_t oem_revision;
+	uint32_t creator_id;
+	uint32_t creator_revision;
+}__attribute__((packed));
+
 /* Locate the RSDP in memory, returning its address. Returns NULL if the RSDP
  * cannot be found. The BIOS's memory must be identity-mapped for this to work. */
 acpi_RSDP *acpi_find_rsdp(void);
