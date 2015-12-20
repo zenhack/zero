c7be44694865b67e9d6256a69399e1f126212121
Ian Denhardt <ian@zenhack.net>
2015-12-19 21:56:27 -0500

    Define more ACPI structures.

--- kernel/platforms/x86/include/kernel/x86/acpi.h
+++ kernel/platforms/x86/include/kernel/x86/acpi.h
@@ -6,6 +6,8 @@
 typedef struct acpi_RSDPv1 acpi_RSDPv1;
 typedef struct acpi_RSDP acpi_RSDP;
 typedef struct acpi_SDTHeader acpi_SDTHeader;
+typedef struct acpi_RSDTBody acpi_RSDTBody;
+typedef struct acpi_SDT acpi_SDT;
 
 struct acpi_RSDPv1 {
 	/* Fields available in acpi 1.0. */
@@ -39,6 +41,21 @@ struct acpi_SDTHeader {
 	uint32_t creator_revision;
 }__attribute__((packed));
 _Static_assert(sizeof(acpi_SDTHeader) == 36, "acpi_SDTHeader is the wrong size.");
+
+struct acpi_RSDTBody {
+	/* In reality, this is a variable length array of pointers to SDTs,
+	 * the length of which can be inferred from the length in our header.
+	 * That's hard to capture in C's typesystem though. This at least
+	 * gives us something we can index (after casting). */
+	uint32_t sdts;
+};
+
+struct acpi_SDT {
+	acpi_SDTHeader header;
+	union {
+		acpi_RSDTBody rsdt;
+	} body;
+}__attribute__((packed));
 
 /* Locate the RSDP in memory, returning its address. Returns NULL if the RSDP
  * cannot be found. The BIOS's memory must be identity-mapped for this to work. */
